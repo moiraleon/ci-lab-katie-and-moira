2eedc4828ebaa5980acd8cce6853974445ebe223
Moira Leon <moira.leon2@gmail.com>
2021-08-02 15:20:22 -0600

    running more tests

--- project.js
+++ project.js
@@ -2,4 +2,14 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-module.exports = getGreeting;
+
+// module.exports = getGreeting;
+module.exports = {
+    getGreeting,
+    returnTwo: function() {return 2},
+    greeting: function(name) {return `Hello ${name}`},
+    add: function(num1,num2) {return num1+num2},
+    multiply: function(num1,num2) {return num1*num2},
+    divide: function(num1,num2) {return num1/num2},
+    subtract: function(num1,num2) {return num1-num2}
+}
--- test.js
+++ test.js
@@ -1,5 +1,31 @@
-const getGreeting = require('./project');
+const {getGreeting,returnTwo,greeting,add,multiply,divide,subtract} = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('entry should return two',function(){
+  expect(returnTwo(3)).toBe(2)
+})
+
+test('greeting should return Hello name',function(){
+  expect(greeting('Dave')).toEqual('Hello Dave')
+})
+
+test('should return sum of num1 and num2', function(){
+  expect(add(5,4)).toEqual(9)
+})
+describe('multiply divide and subtract functions',function(){
+  test('multiply two numbers', function(){
+      expect(multiply(4,2)).toEqual(8);
+      expect(multiply(2,3)).toEqual(6);
+  })
+  test('dividing two numbers', function(){
+      expect(divide(6,2)).toEqual(3);
+      expect(divide(4,2)).toEqual(2);
+  })
+  test('subtracting two numbers', function(){
+      expect(subtract(8,4)).toEqual(4);
+      expect(subtract(9,3)).toEqual(6);
+  })
+})
